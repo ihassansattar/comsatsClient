61f85626f88cf964cf6a0aac1144c0f79a5a53e6
engineeringgeeksquad <engineering.geedsquad@gmail.com>
2020-05-05 00:02:03 +0500

    un comment origal one app.js

--- src/App.js
+++ src/App.js
@@ -1,164 +1,164 @@
-// import React from 'react';
-// import './App.css';
-// import Routes from './Routes';
+import React from 'react';
+import './App.css';
+import Routes from './Routes';
 
-// //   username: "hassan",
-// // };
-// const App = () => (
-//   <div className="App">
-   
-//     {/* <Test/> */}
-//     <Routes />
-//   </div>
-// );
+//   username: "hassan",
+// };
+const App = () => (
+  <div className="App">
+
+    {/* <Test/> */}
+    <Routes />
+  </div>
+);
 
 // export default App;
-import React, { useEffect, useState, useRef } from 'react';
-import './App.css';
-import io from "socket.io-client";
-import Peer from "simple-peer";
-import styled from "styled-components";
+// import React, { useEffect, useState, useRef } from 'react';
+// import './App.css';
+// import io from "socket.io-client";
+// import Peer from "simple-peer";
+// import styled from "styled-components";
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`;
+// const Container = styled.div`
+//   height: 100vh;
+//   width: 100%;
+//   display: flex;
+//   flex-direction: column;
+// `;
 
-const Row = styled.div`
-  display: flex;
-  width: 100%;
-`;
+// const Row = styled.div`
+//   display: flex;
+//   width: 100%;
+// `;
 
-const Video = styled.video`
-  border: 1px solid blue;
-  width: 50%;
-  height: 50%;
-`;
+// const Video = styled.video`
+//   border: 1px solid blue;
+//   width: 50%;
+//   height: 50%;
+// `;
 
-function App() {
-  const [yourID, setYourID] = useState("");
-  const [users, setUsers] = useState({});
-  const [stream, setStream] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
+// function App() {
+//   const [yourID, setYourID] = useState("");
+//   const [users, setUsers] = useState({});
+//   const [stream, setStream] = useState();
+//   const [receivingCall, setReceivingCall] = useState(false);
+//   const [caller, setCaller] = useState("");
+//   const [callerSignal, setCallerSignal] = useState();
+//   const [callAccepted, setCallAccepted] = useState(false);
 
-  const userVideo = useRef();
-  const partnerVideo = useRef();
-  const socket = useRef();
+//   const userVideo = useRef();
+//   const partnerVideo = useRef();
+//   const socket = useRef();
 
-  useEffect(() => {
-    socket.current = io.connect("https://accreditation-services.herokuapp.com/");
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
-      setStream(stream);
-      if (userVideo.current) {
-        userVideo.current.srcObject = stream;
-      }
-    })
+//   useEffect(() => {
+//     socket.current = io.connect("https://accreditation-services.herokuapp.com/");
+//     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+//       setStream(stream);
+//       if (userVideo.current) {
+//         userVideo.current.srcObject = stream;
+//       }
+//     })
 
-    socket.current.on("yourID", (id) => {
-      setYourID(id);
-    })
-    socket.current.on("allUsers", (users) => {
-      setUsers(users);
-    })
+//     socket.current.on("yourID", (id) => {
+//       setYourID(id);
+//     })
+//     socket.current.on("allUsers", (users) => {
+//       setUsers(users);
+//     })
 
-    socket.current.on("hey", (data) => {
-      setReceivingCall(true);
-      setCaller(data.from);
-      setCallerSignal(data.signal);
-    })
-  }, []);
+//     socket.current.on("hey", (data) => {
+//       setReceivingCall(true);
+//       setCaller(data.from);
+//       setCallerSignal(data.signal);
+//     })
+//   }, []);
 
-  function callPeer(id) {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-    });
+//   function callPeer(id) {
+//     const peer = new Peer({
+//       initiator: true,
+//       trickle: false,
+//       stream: stream,
+//     });
 
-    peer.on("signal", data => {
-      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID })
-    })
+//     peer.on("signal", data => {
+//       socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID })
+//     })
 
-    peer.on("stream", stream => {
-      if (partnerVideo.current) {
-        partnerVideo.current.srcObject = stream;
-      }
-    });
+//     peer.on("stream", stream => {
+//       if (partnerVideo.current) {
+//         partnerVideo.current.srcObject = stream;
+//       }
+//     });
 
-    socket.current.on("callAccepted", signal => {
-      setCallAccepted(true);
-      peer.signal(signal);
-    })
+//     socket.current.on("callAccepted", signal => {
+//       setCallAccepted(true);
+//       peer.signal(signal);
+//     })
 
-  }
+//   }
 
-  function acceptCall() {
-    setCallAccepted(true);
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on("signal", data => {
-      socket.current.emit("acceptCall", { signal: data, to: caller })
-    })
+//   function acceptCall() {
+//     setCallAccepted(true);
+//     const peer = new Peer({
+//       initiator: false,
+//       trickle: false,
+//       stream: stream,
+//     });
+//     peer.on("signal", data => {
+//       socket.current.emit("acceptCall", { signal: data, to: caller })
+//     })
 
-    peer.on("stream", stream => {
-      partnerVideo.current.srcObject = stream;
-    });
+//     peer.on("stream", stream => {
+//       partnerVideo.current.srcObject = stream;
+//     });
 
-    peer.signal(callerSignal);
-  }
+//     peer.signal(callerSignal);
+//   }
 
-  let UserVideo;
-  if (stream) {
-    UserVideo = (
-      <Video playsInline muted ref={userVideo} autoPlay />
-    );
-  }
+//   let UserVideo;
+//   if (stream) {
+//     UserVideo = (
+//       <Video playsInline muted ref={userVideo} autoPlay />
+//     );
+//   }
 
-  let PartnerVideo;
-  if (callAccepted) {
-    PartnerVideo = (
-      <Video playsInline ref={partnerVideo} autoPlay />
-    );
-  }
+//   let PartnerVideo;
+//   if (callAccepted) {
+//     PartnerVideo = (
+//       <Video playsInline ref={partnerVideo} autoPlay />
+//     );
+//   }
 
-  let incomingCall;
-  if (receivingCall) {
-    incomingCall = (
-      <div>
-        <h1>{caller} is calling you</h1>
-        <button onClick={acceptCall}>Accept</button>
-      </div>
-    )
-  }
-  return (
-    <Container>
-      <Row>
-        {UserVideo}
-        {PartnerVideo}
-      </Row>
-      <Row>
-        {Object.keys(users).map(key => {
-          if (key === yourID) {
-            return null;
-          }
-          return (
-              
-            <button onClick={() => callPeer(key)}>Call {key}</button>
-          );
-        })}
-      </Row>
-      <Row>
-        {incomingCall}
-      </Row>
-    </Container>
-  );
-}
-export default App;
+//   let incomingCall;
+//   if (receivingCall) {
+//     incomingCall = (
+//       <div>
+//         <h1>{caller} is calling you</h1>
+//         <button onClick={acceptCall}>Accept</button>
+//       </div>
+//     )
+//   }
+//   return (
+//     <Container>
+//       <Row>
+//         {UserVideo}
+//         {PartnerVideo}
+//       </Row>
+//       <Row>
+//         {Object.keys(users).map(key => {
+//           if (key === yourID) {
+//             return null;
+//           }
+//           return (
+
+//             <button onClick={() => callPeer(key)}>Call {key}</button>
+//           );
+//         })}
+//       </Row>
+//       <Row>
+//         {incomingCall}
+//       </Row>
+//     </Container>
+//   );
+// }
+// export default App;
--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const apiPath="https://accreditation-services.herokuapp.com";
+export const apiPath="https://comsatsserver.herokuapp.com";
